d048dff7cd841d27f4a267f74d93cbe9d760a95e
Abdullah <theabdullah224@gmail.com>
2024-12-08 18:07:45 +0500

    changes

--- app/sectionOne/page.tsx
+++ app/sectionOne/page.tsx
@@ -1,9 +1,18 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
 import Banner from "../components/banner/Banner";
 import Cook from '../components/cook/Cook';
 import SaveMoney from '../components/saveMoney/SaveMoney';
+
+import srcimg from "@/public/Resource/couple-teamwork-kitchen.jpg";
+import bgimg from "@/public/Resource/aa890aa8e363918f0c0a94e60eee432c.jpg";
+import workinglady from "@/public/Resource/workinglady.jpg";
+import bgfront from "@/public/Resource/bgfront.jpg";
+
+import ndlady from "@/public/Resource/2ndlady.jpg";
 
 const page = () => {
   return (
