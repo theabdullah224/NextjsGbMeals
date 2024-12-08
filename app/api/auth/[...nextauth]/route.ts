2e1a8814e1a4bdbbfac59af1343505ffe36cbc10
Abdullah <theabdullah224@gmail.com>
2024-12-08 16:31:26 +0500

    changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,6 @@ import NextAuth from 'next-auth';
 import { authOptions } from '../../../auth/auth';
 
 const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST, authOptions };
+export { handler as GET, handler as POST };
 
 
